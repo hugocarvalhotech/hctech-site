5ed5710214d3798685f8c510441e9ade9b6e0098
Hugo Carvalho <hugosuportemu@gmail.com>
2025-05-31 12:53:38 -0300

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ import codeBlueBg from '../assets/images/modernization/code_blue.jpeg';
 import businessMeetingImg from '../assets/images/modernization/business_meeting.jpeg';
 import datacenterRackImg from '../assets/images/modernization/datacenter_rack.jpeg';
 import networkInfrastructureImg from '../assets/images/modernization/network_infrastructure.jpeg';
-import VeeamLogoImg from '../assets/images/modernization/veeam_logo.png';
+import VeeamLogoImg from '../assets/images/modernization/veeam_logo.jpg';
 
 
 const HomePage = () => {
